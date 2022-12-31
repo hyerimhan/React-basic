24b32c7691db5b5a48f12405f4486a8a3efd8dd9
hyerimhan <hnhyerim@gmail.com>
2023-01-01 00:59:33 +0900

    feat: react 1 step

---
+++ src/AppCounter.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+
+export default function AppCounter() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount((prev) => prev + 1);
+  return (
+    <div className="container">
+      <div className="banner">
+        Total Count: {count} {count > 10 ? "fire" : "ice"}
+      </div>
+      <div className="counters">
+        <Counter total={count} onClick={handleClick} />
+        <Counter total={count} onClick={handleClick} />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Products.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+
+export default function Products() {
+  const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((prev) => !prev);
+
+  useEffect(() => {
+    fetch(`data/${checked ? "sale_" : ""}products.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("받아옴");
+        setProducts(data);
+      });
+    return () => {
+      console.log("clean");
+    };
+  }, [checked]);
+
+  return (
+    <>
+      <input
+        id="checkbox"
+        type="checkbox"
+        value={checked}
+        onChange={handleChange}
+      />
+      <label htmlFor="checkbox">Show only hot sale</label>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
