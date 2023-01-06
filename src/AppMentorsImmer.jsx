c766dd7706b3a7a875555047b1d49293e269ca55
hyerimhan <hnhyerim@gmail.com>
2023-01-06 13:16:13 +0900

    feat: useReducer, immer

--- src/AppMentorsImmer.jsx
+++ src/AppMentorsImmer.jsx
@@ -1,25 +1,30 @@
-import React, { useReducer } from "react";
-import personReducer from "./reducer/person-reducer";
+import React from "react";
+import { useImmer } from "use-immer";
 
-export default function AppMentors() {
-  // const [person, setPerson] = useState(initialPerson);
-  const [person, dispatch] = useReducer(personReducer, initialPerson);
+export default function AppMentorsImmer() {
+  const [person, updatePerson] = useImmer(initialPerson);
 
   const handleUpdate = () => {
     const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
     const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
-    dispatch({ type: "updated", prev, current });
+    updatePerson((person) => {
+      const mentor = person.mentors.find((mentor) => mentor.name === prev);
+      mentor.name = current;
+    });
   };
 
   const handleAdd = () => {
     const name = prompt(`멘토의 이름은?`);
     const title = prompt(`멘토의 직함은?`);
-    dispatch({ type: "added", name, title });
+    updatePerson((person) => person.mentors.push({ name, title }));
   };
 
   const handleDelete = () => {
     const name = prompt(`누구를 삭제하고 싶은가요?`);
-    dispatch({ type: "deleted", name });
+    updatePerson((person) => {
+      const index = person.mentors.findIndex((mentor) => mentor.name === name);
+      person.mentors.splice(index, 1);
+    });
   };
   return (
     <div>
---
+++ src/reducer/person-reducer.js
@@ -0,0 +1,35 @@
+// import React from "react";
+
+export default function personReducer(person, action) {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case "updated": {
+      const { prev, current } = action;
+      return {
+        ...person,
+        mentors: person.mentors.map((mentor) => {
+          if (mentor.name === prev) {
+            return { ...mentor, name: current };
+          }
+          return mentor;
+        }),
+      };
+    }
+    case "added": {
+      const { name, title } = action;
+      return {
+        ...person,
+        mentors: [...person.mentors, { name, title }],
+      };
+    }
+    case "deleted": {
+      return {
+        ...person,
+        mentors: person.mentors.filter((mentor) => mentor.name !== action.name),
+      };
+    }
+    default: {
+      throw Error(`알수없는 액션 타입이다: ${action.type}`);
+    }
+  }
+}
