b446e8531d86e9492d7e491e388aca18b52c2e11
hyerimhan <hnhyerim@gmail.com>
2023-01-08 02:58:13 +0900

    feat: class

---
+++ src/AppClass.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Counter from "./basic/components/Counter";
+
+export default class AppClass extends React.Component {
+  state = { count: 0 };
+
+  onClick = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  componentDidMount() {
+    console.log("컴포넌트가 마운트 되었음!");
+  }
+
+  componentWillUnmout() {
+    console.log("컴포넌트가 곧 언마운트 될 예정임!");
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="banner">
+          Total Count: {this.state.count}{" "}
+          {this.state.count > 10 ? "fire" : "ice"}
+        </div>
+        <div className="counters">
+          <Counter total={this.state.count} onClick={this.onClick} />
+          <Counter total={this.state.count} onClick={this.onClick} />
+        </div>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Products from "./basic/components/Products";
+import AppClass from "./AppClass";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Products />
+    <AppClass />
   </React.StrictMode>
 );
 
