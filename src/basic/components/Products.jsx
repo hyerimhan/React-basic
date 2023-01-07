bbe70afdfcfb20015652c6764ea05dd09bfa129d
hyerimhan <hnhyerim@gmail.com>
2023-01-08 02:40:57 +0900

    feat: custom hooks

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,22 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import useProducts from "../../hooks/use-products";
 
 export default function Products() {
-  const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
+  const [loading, error, products] = useProducts({ salesOnly: checked });
   const handleChange = () => setChecked((prev) => !prev);
 
-  useEffect(() => {
-    fetch(`data/${checked ? "sale_" : ""}products.json`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("받아옴");
-        setProducts(data);
-      });
-    return () => {
-      console.log("clean");
-    };
-  }, [checked]);
+  if (loading) return <p>Loading...</p>;
 
+  if (error) return <p>{error}</p>;
   return (
     <>
       <input
